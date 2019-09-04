f290042dc5aa68210bc5e3ad438b9685c758071b
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2019-09-03 21:51:45 -0300

    Correção de import do comando get

--- laguinho/cli.py
+++ laguinho/cli.py
@@ -1,7 +1,6 @@
 import click
-from commands.cmd_get import get 
+from laguinho.commands.cmd_get import get
 
-# Iniciando o cli principal, recebendo uma classe para ser instanciada (cls  = LaguinhoCLI)   
 @click.group()
 def cli():
     """Interface de linha de comando (CLI) para publicar e recuperar dados do laguinho api. """
