2e226715a4536285f2b35f42fc39cd7bf04c5e75
José Renan <joserenansl99@gmail.com>
2019-09-04 13:01:27 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="laguinho",
-    version="1.0",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
