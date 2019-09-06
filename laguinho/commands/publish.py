ee35f710bed19d1991fb9d7bf0452fe55292697f
Arthur Ferrão <arthurdeferrao@gmail.com>
2019-09-06 13:48:23 -0300

    Adiciona help do comando publish

--- laguinho/commands/publish.py
+++ laguinho/commands/publish.py
@@ -1,5 +1,6 @@
 import click
 
-@click.command('publish')
+@click.command('publish', short_help='Publica dataset no laguinho.')
 def publish():
+    """Publica dataset de um repositório do github no laguinho que estará disponível para ser acessado."""
     click.echo('Feature em desenvolvimento...')
