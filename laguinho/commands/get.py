0cdd9b36196dd1e7ba5960738701b568ed329c2f
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2019-09-10 15:37:07 -0300

    Adição de verificação caso o diretório já exista

--- laguinho/commands/get.py
+++ laguinho/commands/get.py
@@ -4,19 +4,19 @@ import os
 import requests
 
 
-## IMPORTANTE!! Refatorar codigo
-## Da pra testar chamando a funcao download_dataset, passando name como sendo o path do github do repo, além de um metadata tipo {"path":"/", "name":"laguinho"}, por exemplo
 @click.command('get', short_help="Retorna dados do repositório.")
 @click.argument('name', required=True, type=str)
 def get(name):
     """Retorna os dados disponiveis de um determinado repositório do github."""
-    click.echo('Recuperando dados de %s' % name)
-    download_dataset({"url":"https://github.com/ArthurFerrao/bot-casper","path":"/backend/src", "name":"teste"})
-    # download_dataset({"url":"https://github.com/lucasaarcoverde/face_recognizer","path":"/data", "name":"gitcli"})    
+    click.echo('Recuperando dados de %s' % name) 
+    metadata = { "url": "https://github.com/OpenDevUFCG/laguinho", "path": "/laguinho", "name": "laguinho"}
+    download_dataset(metadata)
 
 
-# Refatorar -> caso a url n venha com o http pode dar ruim
-# Retorna a url do repositorio na api do github
+def request_github_api(url):
+    return requests.get(url)
+
+
 def create_github_api_url(metadata):
     url_params = metadata['url'].split('/')
     server_idx = url_params.index('github.com')
@@ -27,40 +27,44 @@ def create_github_api_url(metadata):
     github_api = "https://api.github.com/repos/{}/{}/contents{}".format(username, repo, data_path)
     return github_api
 
-# Funcao principal que chama o createDir, alem de criar a pasta onde ficaram os arquivos baixados
+
 def download_dataset(metadata):
-    path = metadata['path']
     current_path = os.getcwd()
     git_url = create_github_api_url(metadata)
     dir_path = current_path +"/" + metadata['name']
-    os.mkdir(dir_path)
-    createDir(git_url, dir_path)
 
-# Funcao recursiva que checa se o elemento é um dir ou não. Caso seja um arquivo, ele chama a funcao
-# de criar um novo arquivo, caso contrário, ele itera dentro dos arquivos do diretorio, onde caso o arquivo
-# seja um dir, ele cria a pasta para ele, além de chamar a funcao recursivamente atualizando os paths
-def createDir(github_url, dir_path):
-    response = requests.get(github_url)
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
+        create_dir(git_url, dir_path)
+        
+    else:
+        print("Diretório '{}' já existe!".format(metadata['name']))
+        metadata['name'] = click.prompt('Qual o nome do novo diretório?')
+        download_dataset(metadata)
+
+
+def create_dir(github_url, dir_path):
+    response = request_github_api(github_url)
     contents = json.loads(response.content)
 
     if  isinstance(contents, list) :
-        # o contents é um diretorio
+
         for content in contents:    
             if content['type'] == 'dir':
                 new_dir_path = dir_path + '/' + content['name']
                 os.mkdir(new_dir_path)
                 new_github_url = github_url + "/" + content['name']
-                createDir(new_github_url, new_dir_path)
+                create_dir(new_github_url, new_dir_path)
             else:
-                createFile(content, dir_path)
+                create_file(content, dir_path)
     else:
-        createFile(contents, dir_path)
+        create_file(contents, dir_path)
 
-# Recebe o content, faz uma requisicao pegando o conteudo dele e escreve no arquivo local
-## IMPORTANTE -> o download_url ta retornando bits, precisa converter pra string 
-def createFile(content, path):
+ 
+def create_file(content, path):
     download_url = content["download_url"]
     print("criando arquivo", content['name'])
-    response = requests.get(download_url)
+
+    response = request_github_api(download_url)
     file = open(path + "/" + content['name'], 'w')
     file.write(response.text)
