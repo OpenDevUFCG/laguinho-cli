6457f65653c2550e15e52a55e400cf8d6fc5024a
José Renan <joserenansl99@gmail.com>
2019-10-13 14:03:16 -0300

    Remove whitespace

--- laguinho/commands/get.py
+++ laguinho/commands/get.py
@@ -5,7 +5,7 @@ import requests
 
 
 @click.command('get', short_help="Retorna dados do repositório.")
-@click.argument('name', required=True, type=str)    
+@click.argument('name', required=True, type=str)
 def get(name):
     """Retorna os dados disponiveis de um determinado repositório do github."""
     click.echo('Recuperando dados de %s' % name)
